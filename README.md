976f0a70e8a2698b0b7a8ea35a67e758fc5ee969
David <2006davidferreira@gmail.com>
2026-03-02 02:15:18 +0000

    a

---
+++ README.md
@@ -0,0 +1,10 @@
+# David Name Flutter App
+
+Minimal Flutter app that displays the name David in the center of the screen.
+
+Run locally:
+
+```bash
+flutter pub get
+flutter run
+```
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'David App',
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Hi! my name is David',
+            style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
